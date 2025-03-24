238f2e5b3d1b5a18e494c9f0fab0bd561c94304a
Ignat Bartolemay <ilnutskyy@gmail.com>
2025-03-24 13:44:48 +0200

    Work some bugs

--- src/main/java/org/example/HttpStatusImageDownloader.java
+++ src/main/java/org/example/HttpStatusImageDownloader.java
@@ -1,7 +1,6 @@
 package org.example;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
-import okhttp3.Response;
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
@@ -14,12 +13,10 @@ public class HttpStatusImageDownloader {
         Request request = new Request.Builder()
                 .url(url)
                 .build();
-        try (Response response = new OkHttpClient().newCall(request).execute()){
-            InputStream inputStream = response.body().byteStream();
+        try (InputStream inputStream= new OkHttpClient().newCall(request).execute().body().byteStream()){
             File file = new File("Image"+String.valueOf(code)+".jpg");
             BufferedImage img = ImageIO.read(inputStream);
             ImageIO.write(img, "jpg", file);
-            inputStream.close();
         } catch (Exception e) {
             throw e;
         }
